030674687ecaf3b26625a5c1e2de033a2069dfae
Amber Jones <amberlovescats14@gmail.com>
2019-09-05 10:37:19 -0500

    get and set

--- src/com/amber/Employee.java
+++ src/com/amber/Employee.java
@@ -1,11 +1,37 @@
 package com.amber;
 
+import java.lang.IllegalAccessException;
+
 public class Employee {
-    public int baseSalary;
-    public int hourlyRate;
+    private int baseSalary;
+    private int hourlyRate;
 
 // because these variables are in our class we don't have to pass them
     public int calculateWage(int extraHours) {
         return baseSalary + (hourlyRate * extraHours);
     }
+
+    //SETTER
+    public void setBaseSalary(int baseSalary) throws IllegalAccessException {
+        if(baseSalary <= 0) throw new IllegalAccessException("Salary cannot be 0 or less");
+        this.baseSalary = baseSalary;
+    }
+
+    //GETTER
+    public int getBaseSalary() {
+        return baseSalary;
+    }
+
+    //SETTER
+    public void setHourlyRate(int hourlyRate) throws IllegalAccessException {
+        if(hourlyRate <=0 ) throw new IllegalAccessException("Hourly rate must be greater than 0");
+        this.hourlyRate = hourlyRate;
+    }
+
+    //GETTER
+    public int getHourlyRate() {
+        return hourlyRate;
+    }
+
+
 }
--- src/com/amber/Main.java
+++ src/com/amber/Main.java
@@ -2,7 +2,7 @@ package com.amber;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException {
         var textBox1 = new TextBox(); // will detect what it is based on the new key
         textBox1.setText("Hello World");
         System.out.println(textBox1.text.toUpperCase());
@@ -11,8 +11,16 @@ public class Main {
         //------------------
 
         var employee = new Employee();
-        employee.baseSalary = 50_000;
-        employee.hourlyRate = 20;
+        //SET
+        employee.setBaseSalary(60_000);
+//        employee.baseSalary = 50_000;
+        //GET
+        employee.getBaseSalary();
+
+        //SET
+        employee.setHourlyRate(30);
+        //GET
+        employee.getHourlyRate();
         int wage = employee.calculateWage(10);
         System.out.println(wage);
 
